ed0d1a07159659cbb3660a024ba7bb699e19662a
Sudharsan Selvaraj <sudharsanselvaraj.c@gmail.com>
2024-07-21 22:11:47 +0530

    Refactor test

--- src/e2eFlutterTest/java/io/appium/java_client/android/CommandTest.java
+++ src/e2eFlutterTest/java/io/appium/java_client/android/CommandTest.java
@@ -3,9 +3,12 @@ package io.appium.java_client.android;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.flutter.commands.ScrollParameter;
 import io.appium.java_client.flutter.commands.WaitParameter;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebElement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CommandTest extends BaseFlutterTest {
 
@@ -21,18 +24,18 @@ public class CommandTest extends BaseFlutterTest {
         WebElement messageField = driver.findElement(messageFieldLocator);
         WebElement toggleButton = driver.findElement(toggleButtonLocator);
 
-        Assertions.assertEquals(messageField.getText(), "Hello world");
+        assertEquals(messageField.getText(), "Hello world");
         toggleButton.click();
-        Assertions.assertEquals(messageField.getText(), "Hello world");
+        assertEquals(messageField.getText(), "Hello world");
 
         WaitParameter waitParameter = new WaitParameter().setLocator(messageFieldLocator);
 
         driver.waitForInVisible(waitParameter);
-        Assertions.assertEquals(0, driver.findElements(messageFieldLocator).size());
+        assertEquals(0, driver.findElements(messageFieldLocator).size());
         toggleButton.click();
         driver.waitForVisible(waitParameter);
-        Assertions.assertEquals(1, driver.findElements(messageFieldLocator).size());
-        Assertions.assertEquals(messageField.getText(), "Hello world");
+        assertEquals(1, driver.findElements(messageFieldLocator).size());
+        assertEquals(messageField.getText(), "Hello world");
     }
 
     @Test
@@ -42,18 +45,18 @@ public class CommandTest extends BaseFlutterTest {
         openScreen("Vertical Swiping");
 
         WebElement firstElement = driver.scrollTillVisible(new ScrollParameter(AppiumBy.flutterText("Java")));
-        Assertions.assertTrue(Boolean.parseBoolean(firstElement.getAttribute("displayed")));
+        assertTrue(Boolean.parseBoolean(firstElement.getAttribute("displayed")));
 
         WebElement lastElement = driver.scrollTillVisible(new ScrollParameter(AppiumBy.flutterText("Protractor")));
-        Assertions.assertTrue(Boolean.parseBoolean(lastElement.getAttribute("displayed")));
-        Assertions.assertFalse(Boolean.parseBoolean(firstElement.getAttribute("displayed")));
+        assertTrue(Boolean.parseBoolean(lastElement.getAttribute("displayed")));
+        assertFalse(Boolean.parseBoolean(firstElement.getAttribute("displayed")));
 
         firstElement = driver.scrollTillVisible(
                 new ScrollParameter(AppiumBy.flutterText("Java"),
                         ScrollParameter.ScrollDirection.UP)
         );
-        Assertions.assertTrue(Boolean.parseBoolean(firstElement.getAttribute("displayed")));
-        Assertions.assertFalse(Boolean.parseBoolean(lastElement.getAttribute("displayed")));
+        assertTrue(Boolean.parseBoolean(firstElement.getAttribute("displayed")));
+        assertFalse(Boolean.parseBoolean(lastElement.getAttribute("displayed")));
     }
 
 }
--- src/e2eFlutterTest/java/io/appium/java_client/android/FinderTests.java
+++ src/e2eFlutterTest/java/io/appium/java_client/android/FinderTests.java
@@ -1,9 +1,10 @@
 package io.appium.java_client.android;
 
 import io.appium.java_client.AppiumBy;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebElement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class FinderTests extends BaseFlutterTest {
@@ -12,32 +13,32 @@ public class FinderTests extends BaseFlutterTest {
     @Test
     public void testFlutterByKey() {
         WebElement userNameField = driver.findElement(AppiumBy.flutterKey("username_text_field"));
-        Assertions.assertEquals("admin", userNameField.getText());
+        assertEquals("admin", userNameField.getText());
         userNameField.clear();
         driver.findElement(AppiumBy.flutterKey("username_text_field")).sendKeys("admin123");
-        Assertions.assertEquals("admin123", userNameField.getText());
+        assertEquals("admin123", userNameField.getText());
     }
 
     @Test
     public void testFlutterByType() {
         WebElement loginButton = driver.findElement(AppiumBy.flutterType("ElevatedButton"));
-        Assertions.assertEquals(loginButton.findElement(AppiumBy.flutterType("Text")).getText(), "Login");
+        assertEquals(loginButton.findElement(AppiumBy.flutterType("Text")).getText(), "Login");
     }
 
     @Test
     public void testFlutterText() {
         WebElement loginButton = driver.findElement(AppiumBy.flutterText("Login"));
-        Assertions.assertEquals(loginButton.getText(), "Login");
+        assertEquals(loginButton.getText(), "Login");
         loginButton.click();
 
-        Assertions.assertEquals(1, driver.findElements(AppiumBy.flutterText("Slider")).size());
+        assertEquals(1, driver.findElements(AppiumBy.flutterText("Slider")).size());
     }
 
     @Test
     public void testFlutterTextContaining() {
         WebElement loginButton = driver.findElement(this.loginButton);
         loginButton.click();
-        Assertions.assertEquals(driver.findElement(AppiumBy.flutterTextContaining("Vertical")).getText(),
+        assertEquals(driver.findElement(AppiumBy.flutterTextContaining("Vertical")).getText(),
                 "Vertical Swiping");
     }
 
@@ -48,7 +49,7 @@ public class FinderTests extends BaseFlutterTest {
         openScreen("Lazy Loading");
 
         WebElement messageField = driver.findElement(AppiumBy.flutterSemanticsLabel("message_field"));
-        Assertions.assertEquals(messageField.getText(),
+        assertEquals(messageField.getText(),
                 "Hello world");
     }
 }
